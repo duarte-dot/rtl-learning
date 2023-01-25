7b841fbed963cad96a57066b25d016326753a040
duarte <gabrieldvr@outlook.com>
2023-01-25 12:12:52 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const textNotFound = screen.getByRole('heading', { name: 'Page requested not found', level: 2 });
+
+    expect(textNotFound).toBeInTheDocument();
+  });
+
+  test('Existe uma imagem com o src https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByRole('img', { src: 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif' });
+
+    expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
